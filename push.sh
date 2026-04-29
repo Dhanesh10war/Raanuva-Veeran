90d4b7cdd6c56465026eed9e5a2b1ebad076e872
Dhanesh10war <dhanesh10war@gmail.com>
2026-04-29 16:41:34 +0530

    feat: enable student audio broadcasting and decouple mic from camera

--- push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git init
-git add .
-git commit -m "Enable student audio broadcasting and decouple mic from camera"
-git branch -M main
-git remote add origin https://github.com/Dhanesh10war/Raanuva-Veeran.git
-git push -u origin main
